c155950a5d8453f0c09be6b37a57034adcaa9fb8
Sreeniketh Madgula <sreeniketh.madgula@gmail.com>
2025-09-13 01:42:55 +0530

    feat: add poetry package manager

--- main.py
+++ main.py
@@ -1,38 +1,7 @@
-import os
-from dotenv import load_dotenv
+from fastapi import FastAPI
 
-from espn_api.football import League, Player
+app = FastAPI()
 
-load_dotenv()
-league_id = int(os.getenv("LEAGUE_ID"))
-year = int(os.getenv("YEAR"))
-espn_s2 = os.getenv("ESPN_S2")
-swid = os.getenv("SWID")
-league = League(league_id=league_id, year=year, espn_s2=espn_s2, swid=swid)
-
-# print(league)
-
-# print(league.box_scores(1))
-
-box_scores = league.box_scores(1)
-
-# print(box_scores[0].home_team.roster)
-
-players = box_scores[0].home_team.roster
-
-cdlamb = players[0]
-
-# print(cdlamb.stats)
-
-cdlambid = cdlamb.playerId
-
-# print(cdlambid)
-
-# print(len(league.free_agents(size=10000)))
-
-for player in league.free_agents(size=10000):
-    if "kyle pitts" in player.name.lower():
-        # print("got kyle", player)
-        
-        # data = league.espn_request.get_player_card(player.playerId, league.finalScoringPeriod)
-        print(player.stats)
+@app.get("/")
+def root():
+    return {"message": "Hello Fantasy Football!"}
